d3f611f42723aff1cbb552a3650966a610e4bb42
netanel <netanel@MINI>
2016-04-12 00:08:15 +0300

    Finished MyComplex

--- 822.74/src/com/netanel/complex/MyComplex.java
+++ 822.74/src/com/netanel/complex/MyComplex.java
@@ -3,15 +3,23 @@ package com.netanel.complex;
 public class MyComplex {
 	private double real;
 	private double imag;
-	
-	//Constructor
+
+	//
+	// Constructor
+	//
+	public MyComplex() {
+		real = 0.0;
+		imag = 0.0;
+	}
 	public MyComplex (double real, double imag)
 	{
 		this.real = real;
 		this.imag = imag;
 	}
 	
-	//Getters
+	//
+	// Getters
+	//
 	public double getReal()
 	{
 		return real;
@@ -22,7 +30,9 @@ public class MyComplex {
 		return imag;
 	}
 	
-	//Setters
+	//
+	// Setters
+	//
 	public void setReal(double real)
 	{
 		this.real = real;
@@ -32,41 +42,117 @@ public class MyComplex {
 	{
 		this.imag = imag;
 	}
-	
-	//toString
+
+	// toString
 	public String toString()
 	{
-		return "(" + real + " + " + imag + ")";
+		return "(" + real + " + " + imag + "i)";
 	}
 	
-	//Methods
+	//
+	// Methods
+	//
+	
+	// Sets the complex values
 	public void setValue(double real, double imag)
 	{
 		setReal(real);
 		setImag(imag);
 	}
 	
+	// Checks if is real
 	public boolean isReal()
 	{
 		return (imag == 0);
 	}
 	
+	// Checks if is imaginary
 	public boolean isImaginary()
 	{
 		return (imag != 0);
 	}
 	
+	// Checks equality to real/imaginary pair 
 	public boolean equals(double real, double imag)
 	{
 		return (this.real == real && this.imag == imag);
 	}
 	
+	// Checks equality to another complex
 	public boolean equals(MyComplex another)
 	{
-//		return (this.real == another.real && this.imag == another.imag);
-//		return (this.real == another.getReal() && this.imag == another.getImag());
+		return (this.real == another.real && this.imag == another.imag);
+
+	}
+	
+	// Returns the magnitude of this complex number
+	public double magnitude()
+	{
+		return Math.sqrt(real*real + imag*imag);
+	}
+	
+	// Returns the argument of this complex number in radians
+	public double argument() {
+		return Math.atan2(imag, real);
+	}
+	
+	//  Adds the given MyComplex instance (called right), into this instance and returns this instance
+	public MyComplex add(MyComplex right)
+	{
+		this.setValue(this.real + right.real, this.imag + right.imag);
+		return this;
+	}
+	
+	//  Subtract the given MyComplex instance (called right), from this instance and returns this instance
+	public MyComplex subtract(MyComplex right)
+	{
+		this.setValue(this.real - right.real, this.imag - right.imag);
+		return this;
+	}
+	
+	// Adds this instance with the given MyComplex instance called right, and returns a new MyComplex instance containing the result
+	public MyComplex addNew(MyComplex right)
+	{
+		return new MyComplex(this.real + right.real, this.imag + right.imag);
+	}
+	
+	// Subtract this instance with the given MyComplex instance called right, and returns a new MyComplex instance containing the result
+	public MyComplex subtractNew(MyComplex right)
+	{
+		return new MyComplex(this.real - right.real, this.imag - right.imag);
+	}
+	
+	// Multiplies this instance with the given MyComplex instance right, and keeps the result in this instance, and returns this instance
+	//	(a + bi) * (c + di) = (ac - bd) + (ad + bc)i
+	public MyComplex multiply(MyComplex right)
+	{
+		double a = this.real;
+		double b = this.imag;
+		double c = right.real;
+		double d = right.imag;
 		
-		return (this.toString() == another.toString());
-	}	
+		this.setValue(a*c - b*d, a*d + b*c);
+		return this; 	
+	}
+	
+	// Divides this instance with the given MyComplex instance right, and keeps the result in this instance, and returns this instance\
+	// (a + bi) / (c + di) = [(a + bi) * (c – di)] / (c*c + d*d)
+	public MyComplex divide(MyComplex right)
+	{
+		double a = this.real;
+		double b = this.imag;
+		double c = right.real;
+		double d = right.imag;
+		
+		this.setValue((a*c+b*d)/(c*c+d*d), (b*c-a*d)/(c*c+d*d));
+		return this;
+	}
+	
+	// Operates on this instance and returns this instance containing the complex conjugate
+	public MyComplex conjugate()
+	{
+		this.setValue(real, -imag);
+		return this;
+	}
 	
 }
--- 822.74/src/com/netanel/complex/TestMyComplex.java
+++ 822.74/src/com/netanel/complex/TestMyComplex.java
@@ -3,14 +3,18 @@ package com.netanel.complex;
 public class TestMyComplex {
 	public static void main(String[] args) {
 		MyComplex c1 = new MyComplex(10, 5);
-		MyComplex c2 = new MyComplex(10, 5);
+		MyComplex c2 = new MyComplex(12, 3);
+		MyComplex c3 = new MyComplex(1, 1);
 		
-		String s1 = c1.toString();
-		String s2 = c2.toString();
+		System.out.println(c1.equals(c2));
+		System.out.println(c1.equals(c3));
 		
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s1 == s2);
+		c1.subtract(c2);
+		System.out.println(c1);
+		c1.add(c3);
+		System.out.println(c1);
+		
+		System.out.println(c1.conjugate());
 		
 	}
 }
