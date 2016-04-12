9df2ca424d466bea460cadf7bcd2ded44c1651db
attaln <attaln@ISSAATTALNL7C.corp.emc.com>
2016-04-12 12:25:00 +0300

    done with next methods

--- 822.74/src/com/netanel/date/MyDate.java
+++ 822.74/src/com/netanel/date/MyDate.java
@@ -20,76 +20,25 @@ public class MyDate {
 		setDate(year, month, day);
 	}
 	
-	
 	//
-	// Methods
+	// Getters
 	//
 	
-	
-	public static boolean isLeapYear(int year)
-	// returns true if the given year is a leap year.
-	// A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
-	{
-		return (year % 4 == 0 && year % 100 !=0) || (year % 400 == 0);
+	public int getYear() {
+		return year;
 	}
-	
 
-	public static boolean isValidDate(int year, int month, int day)
-	// returns true if the given year, month, and day constitute a valid date.
-	// Assume that year is between 1 and 9999, month is between 1 (Jan) to 12 (Dec) and day shall be 
-	// between 1 and 28|29|30|31 depending on the month and whether it is a leap year on Feb	
-	{
-		if (!(month >= 1 && month <= 12))
-		{
-			return false;
-		}
-		int lastDay = daysInMonths[month-1];
-		//Feb of a leap year = 29
-		if (month == 2 && isLeapYear(year))
-		{
-			lastDay = 29;
-		}
-		return ((year >= 1 && year <= 9999) && (day >= 1 && day <= lastDay));
+	public int getMonth() {
+		return month;
+	}
+
+	public int getDay() {
+		return day;
 	}
 	
-	public static int getDayOfWeek(int year, int month, int day)
-	// returns the day of the week, where 0 for Sun, 1 for Mon, ..., 6 for Sat, for the given date
-	{
-		int dayOfWeek = -1;
-		
-		// century number rotates between 4,2,0,6
-		int centuryNumber = 0;
-		// 0-11 are months on normal year, 12-13 are Jan and Feb on leap year
-		int[] monthNumberArr = {0,3,3,6,1,4,6,2,5,0,3,5,6,2};
-		int monthNumber;
-		int lastTwoDigits = year % 100;
-		
-		switch (year/100 % 4)
-		{
-		case 1: centuryNumber = 4;
-				break;
-		case 2: centuryNumber = 2;
-				break;
-		case 3: centuryNumber = 0;
-				break;
-		case 0: centuryNumber = 6;
-				break;
-		}
-		
-		if (isLeapYear(year))
-		{
-			monthNumber = monthNumberArr[month+11];
-		}
-		else
-		{
-			monthNumber = monthNumberArr[month-1];
-		}
-		
-		dayOfWeek = centuryNumber + lastTwoDigits + lastTwoDigits/4 + monthNumber + day;
-		
-		return dayOfWeek;
-	}
-	
+	//
+	// Getters
+	//
 	public void setDate(int year, int month, int day)
 	{
 		if (isValidDate(year, month, day))
@@ -141,10 +90,145 @@ public class MyDate {
 	}
 	
 	//
+	// Methods
+	//
+	
+	public static boolean isLeapYear(int year)
+	// returns true if the given year is a leap year.
+	// A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
+	{
+		return (year % 4 == 0 && year % 100 !=0) || (year % 400 == 0);
+	}
+	
+
+	public static boolean isValidDate(int year, int month, int day)
+	// returns true if the given year, month, and day constitute a valid date.
+	// Assume that year is between 1 and 9999, month is between 1 (Jan) to 12 (Dec) and day shall be 
+	// between 1 and 28|29|30|31 depending on the month and whether it is a leap year on Feb	
+	{
+		if (!(month >= 1 && month <= 12))
+		{
+			return false;
+		}
+		int lastDay = daysInMonths[month-1];
+		//Feb of a leap year = 29
+		if (month == 2 && isLeapYear(year))
+		{
+			lastDay = 29;
+		}
+		return ((year >= 1 && year <= 9999) && (day >= 1 && day <= lastDay));
+	}
+	
+	public static int getDayOfWeek(int year, int month, int day)
+	// returns the day of the week, where 0 for Sun, 1 for Mon, ..., 6 for Sat, for the given date
+	{
+		int dayOfWeek = -1;
+		
+		// century number rotates between 4,2,0,6
+		int centuryNumber = 0;
+		// 0-11 are months on normal year, 12-13 are Jan and Feb on leap year
+		int[] monthNumberArr = {0,3,3,6,1,4,6,2,5,0,3,5,6,2};
+		int monthNumber;
+		int lastTwoDigits = year % 100;
+		
+		switch (year/100 % 4)
+		{
+		case 1: centuryNumber = 4;
+				break;
+		case 2: centuryNumber = 2;
+				break;
+		case 3: centuryNumber = 0;
+				break;
+		case 0: centuryNumber = 6;
+				break;
+		}
+		
+		if (isLeapYear(year) && (month == 1 || month == 2))
+		{
+			monthNumber = monthNumberArr[month+11];
+		}
+		else
+		{
+			monthNumber = monthNumberArr[month-1];
+		}
+		
+		dayOfWeek = (centuryNumber + lastTwoDigits + lastTwoDigits/4 + monthNumber + day) % 7;
+		
+		return dayOfWeek;
+	}
+	
+	public MyDate nextDay()
+	// Update this instance to the next day and return this instance
+	// Take note that nextDay() for 31 Dec 2000 shall be 1 Jan 2001.
+	{	
+		if (day >= daysInMonths[month-1])
+		{
+			if (isLeapYear(year) && month == 2 && day == 28)
+			{
+				day = 29;
+			}
+			else
+			{
+				day = 1;
+				nextMonth();	
+			}
+			
+		}
+		else
+		{
+			day++;
+		}
+		return this;
+	}
+	
+	public MyDate nextMonth()
+	// Update this instance to the next month and return this instance. 
+	// Take note that nextMonth() for 31 Oct 2012 shall be 30 Nov 2012.
+	{
+		if (month == 12)
+		{
+			month = 1;
+			nextYear();
+		}
+		else
+		{
+			month++;
+		}
+		
+		if (day > daysInMonths[month-1])
+		{
+			setDay(daysInMonths[month-1]);
+		}
+		return this;
+	}
+	
+	public MyDate nextYear()
+	// Update this instance to the next year and return this instance.
+	// Take note that nextYear() for 29 Feb 2012 shall be 28 Feb 2013
+	{
+		if (year == 9999)
+		{
+			System.out.println("Year out of range!");
+			return this;			
+		}
+		else
+		{
+			year++;
+		}
+		
+		if (!isLeapYear(year) && month == 2 && day == 29)
+		{
+			// Or just day == 28?
+			setDay(28);
+		}
+		return this;
+	}
+	
+	//
 	// toString
 	//
 	public String toString()
 	{
-		return this.day + "/" + this.month + "/" + this.year;
+		return strDays[getDayOfWeek(year, month, day)] + " " + day + " " + strMonths[month-1] + " " + year;
 	}
 }
--- 822.74/src/com/netanel/date/TestMyDate.java
+++ 822.74/src/com/netanel/date/TestMyDate.java
@@ -2,7 +2,14 @@ package com.netanel.date;
 
 public class TestMyDate {
 	public static void main(String[] args) {
-		MyDate d1 = new MyDate(1995, 12, 15);
-		System.out.println(d1);
+		MyDate d1 = new MyDate(2016, 2, 12);
+//		System.out.println(d1.getDayOfWeek(2012, 2, 17));
+		//System.out.println(d1);
+//		//System.out.println(d1.nextDay());
+//		
+		for (int i = 0; i <1000; i++) {
+			System.out.println(d1.nextDay());
+		}
+		
 	}
 }
