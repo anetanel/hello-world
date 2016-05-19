572d6115ee8766e939ffe597fb6f0007a0452c34
attaln <attaln@ISSAATTALNL7C.corp.emc.com>
2016-05-19 13:53:16 +0300

    imdb exceptions

---
+++ 822.74/src/com/netanel/imdb/ImdbException.java
@@ -0,0 +1,30 @@
+package com.netanel.imdb;
+
+public class ImdbException extends Exception{
+
+	public ImdbException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImdbException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImdbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImdbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImdbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
